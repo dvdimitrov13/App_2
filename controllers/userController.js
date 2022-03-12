7e008298c98062c974e5cfd8bbf675ec64889221
Dimitar <dvdimitrov13@gmail.com>
2022-03-12 21:55:25 +0200

    Creating posts and publishing to DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
 const e = require('connect-flash')
+const { nextTick } = require('process')
 const User = require('../models/User')
+
+exports.mustBeLoggedIn = function(req, res, next) {
+    if (req.session.user) {
+        next()
+    } else {
+        req.flash("errors", "You must be logged in to perform that action")
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }
+}
 
 exports.home = function(req, res) {
     if (req.session.user) {
-        res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
+        res.render('home-dashboard')
     } else {
         res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
