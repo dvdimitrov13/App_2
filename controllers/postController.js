e7d13e88da0b0b95a5ebf9230fd648c16ac58dec
Dimitar <dvdimitrov13@gmail.com>
2022-03-14 19:35:53 +0200

    Implemented Post edit

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,9 +17,47 @@ exports.create = function(req, res) {
 
 exports.viewSingle = async function(req, res) {
     try {
-        let post = await Post.findSingleById(req.params.id)
+        let post = await Post.findSingleById(req.params.id, req.visitorId)
         res.render('single-post-screen', {post: post})
     } catch {
         res.render('404')
     }
+}
+
+exports.viewEditScreen = async function(req, res) {
+    try {
+        let post = await Post.findSingleById(req.params.id)
+        res.render('edit-post', {post: post})
+    } catch {
+        res.render('404')
+    }
+}
+
+exports.edit = function(req, res) {
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status) => {
+        // the post was succesfully updated
+        // or the user is the owner but there were validation errors
+        if (status == "success") {
+            // post was updated in DB
+            req.flash("success","Post successfully updated!")
+            req.session.save(function() {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        } else {
+            post.errors.forEach(function(err) {
+                req.flash("errors", err)
+            })
+            req.session.save(function() {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        }
+    }).catch(() => {
+        // a post with the requested id doesnt exist
+        //or if the current visitor is not the owner of the post
+        req.flash("errors", "You do not have permission to access that page!")
+        req.session.save(function() {
+            res.redirect("/")
+        })
+    })
 }
